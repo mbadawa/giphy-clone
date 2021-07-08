1ad1eabdefdc92f375124ce15e9cac432acc69ee
Mohamad Badawa <7badawa@gmail.com>
2021-07-08 00:11:10 -0400

    Update format

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
+function apiRequest() {
+	var userInput = document.querySelector(".search").value;
+	console.log(userInput);
+	var apiKey = "RtWkq4uMtQ4Jnu4a2v1ktXDEwgdQOzHj";
+	var url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${userInput}d&limit=25&offset=0&rating=g&lang=en`;
 
-function apiRequest(){
-        var userInput = document.querySelector('.search').value
-        console.log(userInput);
-        var apiKey = "RtWkq4uMtQ4Jnu4a2v1ktXDEwgdQOzHj"
-        var url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${userInput}d&limit=25&offset=0&rating=g&lang=en`
-
-        fetch(url)
-        .then(function(data){
-            return data.json()
-        })
-        .then(function(json){
-            for (let i = 0; i < json.data.length; i++){
-                var imgPath = json.data[i].images.fixed_height.url;
-                var img = document.createElement("img");
-                img.setAttribute("src", imgPath);
-                document.querySelector('.pictures').appendChild(img);
-            }
-        })
+	fetch(url)
+		.then(function (data) {
+			return data.json();
+		})
+		.then(function (json) {
+			for (let i = 0; i < json.data.length; i++) {
+				var imgPath = json.data[i].images.fixed_height.url;
+				var img = document.createElement("img");
+				img.setAttribute("src", imgPath);
+				document.querySelector(".pictures").appendChild(img);
+			}
+		});
 }
-document.querySelector('.search-btn').addEventListener("click", apiRequest)
+document.querySelector(".search-btn").addEventListener("click", apiRequest);
